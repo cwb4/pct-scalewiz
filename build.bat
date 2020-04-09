d2d8c0f1a47218fdf45d3e68d8911c5c28b39677
teauxfu <alexmw777@gmail.com>
2020-04-09 10:57:36 -0500

    update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 CALL scripts\activate
-CALL python -m pyinstaller -y -F -w -i "images\wizard.ico" "C:/Users/P/Documents/GitHub/pct-scalewiz/core.py"
+CALL python pyinstaller -y -F -w -i "images\wizard.ico" "C:/Users/P/Documents/GitHub/pct-scalewiz/core.py"
