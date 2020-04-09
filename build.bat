c7e825002a58f6afe988ffe02b3beb213a800ea2
teauxfu <alexmw777@gmail.com>
2020-04-09 10:59:48 -0500

    generalize build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 CALL scripts\activate
-CALL python -m PyInstaller -y -F -w -i "images\wizard.ico" "C:/Users/P/Documents/GitHub/pct-scalewiz/core.py"
+CALL python -m PyInstaller -y -F -w -i "images\wizard.ico" "core.py"
