3d91e8f1c1faae0b494d47bf51bb960a7ddf49ed
teauxfu <alexmw777@gmail.com>
2020-04-13 00:13:01 -0500

    move all non-GUI related code out of mainwindow.py

--- source/mainwindow.py
+++ source/mainwindow.py
@@ -20,41 +20,25 @@ from tkinter import ttk
 import time  # sleeping
 from winsound import Beep  # beeping when the test ends
 
+from experiment import Experiment
 from menubar import MenuBar
 
 
 class MainWindow(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
-        self.parent = parent
-
-        # define test parameters
-        self.port1 = tk.StringVar()  # COM port for pump1
-        self.port2 = tk.StringVar()  # COM port for pump2
-        self.timelimit = tk.DoubleVar()
-        self.failpsi = tk.IntVar()
-        self.chem = tk.StringVar()
-        self.conc = tk.StringVar()
-        self.savepath = tk.StringVar()  # output directory
-        self.project = tk.StringVar()  # used for window title
-        self.plotpsi = tk.StringVar()  # for which pump's data to plot
+        self.plotpsi = tk.StringVar()
         self.plotstyle = tk.StringVar()
-
-        # set initial
-        self.paused = True
-        self.timelimit.set(90)
-        self.failpsi.set(1500)
-        self.savepath.set(os.getcwd())
         self.plotpsi.set('PSI 2')
         self.plotstyle.set('seaborn-colorblind')
-        self.outfile = f"{self.chem.get()}_{self.conc.get()}.csv"
         self.build_window()
+        self.findcoms()
 
-    def build_window(self):
+    def build_window(self) -> None:
         """Make all the tkinter widgets"""
         self.menu = MenuBar(self)
         # build the main frame
-        self.tstfrm = tk.Frame(self.parent)
+        self.tstfrm = tk.Frame(self)
         self.entfrm = tk.LabelFrame(self.tstfrm, text="Test parameters")
         # this spacing is to avoid using multiple labels
         self.outfrm = tk.LabelFrame(self.tstfrm,
@@ -65,44 +49,43 @@ class MainWindow(tk.Frame):
         self.p1 = ttk.Entry(
             master=self.entfrm,
             width=14,
-            textvariable=self.port1,
             justify=tk.CENTER
             )
         self.p2 = ttk.Entry(
             master=self.entfrm,
             width=14,
-            textvariable=self.port2,
             justify=tk.CENTER
             )
         self.tl = ttk.Entry(
             master=self.entfrm,
             width=30,
             justify=tk.CENTER,
-            textvariable=self.timelimit
             )
         self.fp = ttk.Entry(
             master=self.entfrm,
             width=30,
             justify=tk.CENTER,
-            textvariable=self.failpsi
             )
         self.ch = ttk.Entry(
             master=self.entfrm,
             width=30,
             justify=tk.CENTER,
-            textvariable=self.chem
             )
+        self.ch.focus_set()  # move the cursor here for convenience
         self.co = ttk.Entry(
             master=self.entfrm,
             width=30,
             justify=tk.CENTER,
-            textvariable=self.conc
             )
         self.strtbtn = ttk.Button(
             master=self.entfrm,
             text="Start",
             command=self.init_test
             )
+
+        # default values for convenience (maybe store in config file instead)
+        self.tl.insert(0, '90')
+        self.fp.insert(0, '1500')
 
         # grid entry labels into self.entfrm
         self.comlbl = ttk.Label(master=self.entfrm, text="COM ports:")
@@ -124,6 +107,10 @@ class MainWindow(tk.Frame):
             text="Concentration:"
             ).grid(row=4, sticky=tk.E)
 
+        # widget bindings for user convenience
+        self.co.bind("<Return>", self.init_test)
+        self.comlbl.bind("<Button-1>", lambda _: self.findcoms())
+
         # grid entries into self.entfrm
         self.p1.grid(row=0, column=1, sticky=tk.E, padx=(9, 1))
         self.p2.grid(row=0, column=2, sticky=tk.W, padx=(5, 3))
@@ -136,7 +123,7 @@ class MainWindow(tk.Frame):
         for col in range(cols[0]):
             self.entfrm.grid_columnconfigure(col, weight=1)
 
-        # build self.outfrm PACK
+        # build self.outfrm packed
         scrollbar = tk.Scrollbar(self.outfrm)
         self.dataout = tk.Text(
             master=self.outfrm,
@@ -150,7 +137,7 @@ class MainWindow(tk.Frame):
         scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
         self.dataout.pack(fill=tk.BOTH)
 
-        # build self.cmdfrm 4x3 GRID
+        # build self.cmdfrm 4x3 grid
         self.runbtn = ttk.Button(
             master=self.cmdfrm,
             text="Run",
@@ -165,6 +152,8 @@ class MainWindow(tk.Frame):
             )
         self.runbtn.grid(row=1, column=1, padx=5, pady=2, sticky=tk.W)
         self.endbtn.grid(row=1, column=2, padx=5, pady=2, sticky=tk.E)
+
+        # a pair of Radiobuttons to choose which column of data to plot
         tk.Label(
             master=self.cmdfrm,
             text="Select data to plot:"
@@ -183,16 +172,16 @@ class MainWindow(tk.Frame):
             ).grid(row=0, column=2, padx=5)
 
         # disable the controls to prevent starting test w/o parameters
-        if self.paused:
-            for child in self.cmdfrm.winfo_children():
-                child.configure(state="disabled")
+        for child in self.cmdfrm.winfo_children():
+            child.configure(state="disabled")
 
         # set up the plot area
         self.pltfrm = tk.LabelFrame(
             master=self.tstfrm,
-            text=("Style: " + self.plotstyle.get())
+            text=(f"Style: {self.plotstyle.get()}")
             )
 
+        # matplotlib objects
         self.fig, self.ax = plt.subplots(figsize=(7.5, 4), dpi=100)
         plt.subplots_adjust(left=0.10, bottom=0.12, right=0.97, top=0.95)
         # TODO: explicitly clarify some of these args
@@ -207,150 +196,46 @@ class MainWindow(tk.Frame):
         self.pltfrm.grid(row=0, column=1, rowspan=3, sticky=tk.NSEW, padx=2)
         self.outfrm.grid(row=1, column=0, sticky=tk.NSEW, pady=2)
         self.cmdfrm.grid(row=2, column=0, sticky=tk.NSEW, pady=2)
-
-        # widget bindings
-        self.co.bind("<Return>", self.init_test)
-        self.comlbl.bind("<Button-1>", lambda _: self.findcoms())
-
         self.tstfrm.grid(padx=3)
-        self.findcoms()
-        self.ch.focus_set()
 
-    def findcoms(self):
+    def findcoms(self) -> None:
         """Looks for COM ports and disables the controls if two aren't found"""
-        self.to_log("Finding COM ports...")
         ports = ["COM" + str(i) for i in range(15)]
         useports = []
         for i in ports:
-            try:
-                if serial.Serial(i).is_open:
-                    self.to_log(f"Found an open port at {i}")
-                    useports.append(i)
-                    serial.Serial(i).close
-            except serial.SerialException:
-                pass
+            if serial.Serial(i).is_open:
+                useports.append(i)
+                serial.Serial(i).close
+
         if len(useports) < 2:
             self.to_log("Not enough COM ports found...")
             self.to_log("Click 'COM ports:' to try again.")
             useports = ["??", "??"]
+
         try:
-            self.port1.set(useports[0])
-            self.port2.set(useports[1])
-            if self.port1.get() == "??" or self.port2.get() == "??":
+            self.p1.insert(0, useports[0])
+            self.p2.insert(0, useports[1])
+
+            if self.p1.get() == "??" or self.p2.get() == "??":
                 self.strtbtn['state'] = ['disable']
             else:
                 self.strtbtn['state'] = ['enable']
-
+                # TODO: clean this up
         except IndexError:
             pass
         except AttributeError:
             pass
 
-    def init_test(self):
-        """Collects all the user data from the GUI widgets"""
-        self.port1.set(self.p1.get())
-        self.port2.set(self.p2.get())
-        self.timelimit.set(self.tl.get())
-        self.failpsi.set(self.fp.get())
-        self.chem.set(self.ch.get())
-        self.conc.set(self.co.get())
+    def init_test(self) -> None:
+        """init an Experiment object as an attribute of MainWindow"""
+        self.test = Experiment(self)
 
-        self.outfile = f"{self.chem.get()}_{self.conc.get()}.csv"
-        self.psi1, self.psi2, self.elapsed = 0, 0, 0
-        # the timeout values are an alternative to using TextIOWrapper
-        self.pump1 = serial.Serial(self.port1.get(), timeout=0.01)
-        print(f"Opened a port at {self.port1.get()}")
-        self.pump2 = serial.Serial(self.port2.get(), timeout=0.01)
-        print(f"Opened a port at {self.port2.get()}")
-
-        # set up output file
-        outputpath = os.path.join(self.savepath.get(), self.outfile)
-        print(f"Creating output file at {outputpath}")
-        with open(os.path.join(self.savepath.get(), self.outfile), "w") as f:
-            csv.writer(f, delimiter=',').writerow(
-                                                    [
-                                                     "Timestamp",
-                                                     "Seconds",
-                                                     "Minutes",
-                                                     "PSI 1",
-                                                     "PSI 2"
-                                                     ]
-                                                  )
-        # disable the entries for test parameters
-        for child in self.entfrm.winfo_children():
-            child.configure(state="disabled")
-        # enable the commands for starting/stopping the test
-        for child in self.cmdfrm.winfo_children():
-            child.configure(state="normal")
-
-    def to_log(self, msg):
+    def to_log(self, msg) -> None:
         """Logs a message to the Text widget in MainWindow's outfrm"""
         self.dataout['state'] = 'normal'
         self.dataout.insert('end', f"{msg}" + "\n")
         self.dataout['state'] = 'disabled'
         self.dataout.see('end')
-
-    def end_test(self):
-        """Stops the pumps and closes their COM ports, then swaps the button
-        states for the entfrm and cmdfrm widgets"""
-        self.paused = True
-        self.pump1.write('st'.encode())
-        self.pump1.close()
-        self.pump2.write('st'.encode())
-        self.pump2.close()
-        msg = "The test finished in {0:.2f} minutes".format(self.elapsed/60)
-        self.to_log(msg)
-        for child in self.entfrm.winfo_children():
-            child.configure(state="normal")
-        for child in self.cmdfrm.winfo_children():
-            child.configure(state="disabled")
-
-    def run_test(self):
-        """Submits a test loop to the thread_pool_executor"""
-        if self.paused:
-            self.pump1.write('ru'.encode())
-            self.pump2.write('ru'.encode())
-            self.paused = False
-            # let the pumps warm up before we start recording data
-            time.sleep(3)
-            self.parent.thread_pool_executor.submit(self.take_reading)
-
-    def take_reading(self):
-        """loop to be handled by the thread_pool_executor"""
-        starttime = datetime.now()
-        while (
-         (self.psi1 < self.failpsi.get() or self.psi2 < self.failpsi.get())
-         and self.elapsed < self.timelimit.get()*60
-         and not self.paused
-         ):
-            rn = time.strftime("%I:%M:%S", time.localtime())
-            self.pump1.write("cc".encode())
-            self.pump2.write("cc".encode())
-            time.sleep(0.1)
-            self.psi1 = int(self.pump1.readline().decode().split(',')[1])
-            self.psi2 = int(self.pump2.readline().decode().split(',')[1])
-            thisdata = [
-                        rn,
-                        self.elapsed,  # as seconds
-                        '{0:.2f}'.format(self.elapsed/60),  # as minutes
-                        self.psi1,
-                        self.psi2
-                        ]
-
-            outputpath = os.path.join(self.savepath.get(), self.outfile)
-            with open((outputpath), "a", newline='') as f:
-                csv.writer(f, delimiter=',').writerow(thisdata)
-            nums = ((self.elapsed/60), self.psi1, self.psi2)
-            logmsg = ("{0:.2f} min, {1} psi, {2} psi".format(nums))
-            self.to_log(logmsg)
-            time.sleep(0.9)
-            self.elapsed = (datetime.now() - starttime).seconds
-
-        if not self.paused:
-            self.end_test()
-            for i in range(3):
-                Beep(750, 500)
-                time.sleep(0.5)
 
     def animate(self, i):
         """The animation function for the current test's data"""
