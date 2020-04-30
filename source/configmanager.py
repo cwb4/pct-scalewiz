e851f35361545b7117a703bee0469f503939d110
teauxfu <alexmw777@gmail.com>
2020-04-30 12:37:57 -0500

    add notes

--- source/configmanager.py
+++ source/configmanager.py
@@ -22,6 +22,10 @@ Methods:
     * ConfigManager.as_dict(ConfigParser) -> dict : returns ConfigParser object
      as a dict
 
+Notes:
+    * tries to treat very long dict values as lists if they contain commas;
+     the contents are returned to string when writing to file. see line 146
+
 """
 
 
