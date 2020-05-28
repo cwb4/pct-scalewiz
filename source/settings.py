ef64b2e804a18f51de6d29a4a01848a6540930ba
teauxfu <alexmw777@gmail.com>
2020-05-28 16:37:54 -0500

    combobox bg -> white always

--- source/settings.py
+++ source/settings.py
@@ -94,6 +94,7 @@ class ConfigManager(tk.Toplevel):
             test_frm,
             values=["PSI 1", "PSI 2"],
             state='readonly',
+            background='white',
             width=29
         )
         self.proj_dir = ttk.Entry(
