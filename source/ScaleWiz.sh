525ade11542496d16207d645ab1cd7fa2e510682
teauxfu <alexmw777@gmail.com>
2020-06-03 11:36:36 -0500

    remove shell scripts from repo

--- source/ScaleWiz.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-cd pct-scalewiz/source/
-python3 core.py
--- source/hard_sync.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-rclone -v sync remote:"#Scale Test Data" "pct-scalewiz/Scale Test Data"
-echo "Done syncing"
-read -n 1 -s
